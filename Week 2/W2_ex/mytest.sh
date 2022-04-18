4ada9c02ae07ca66ded759554c2dc0cce58ad470
MarcoHoang <103943607+MarcoHoang@users.noreply.github.com>
2022-04-18 22:28:35 +0700

    Create mytest.sh

---
+++ Week 2/W2_ex/mytest.sh
@@ -0,0 +1,7 @@
+#! / bin / bash
+máy quét ../test/example1.kpl | diff ../test/result1.txt -
+máy quét ../test/example2.kpl | diff ../test/result2.txt -
+máy quét ../test/example3.kpl | diff ../test/result3.txt -
+máy quét ../test/example4.kpl | diff ../test/result4.txt -
+máy quét ../test/example5.kpl | diff ../test/result5.txt -
+máy quét ../test/example6.kpl | khác ../test/result6.txt -
